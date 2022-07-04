584cf375e864b41d3ff3b21899dbc5c9ee38c2c7
prabincankod <prabincankod@gmail.com>
2022-07-04 19:10:01 +0545

    initial commit

---
+++ webapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    message = "hello world from views.py from vercel"
+    return HttpResponse(message)
