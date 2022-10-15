c27709b0641a27b426bf28b3a04a8134c0da8731
prabindoescoding <115853945+prabindoescoding@users.noreply.github.com>
2022-10-15 12:23:32 +0545

    django init

--- webapp/views.py
+++ webapp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    message = "hello world from views.py from vercel from matinsoftech"
+    message = "hello world from views.py from vercel and hacktoberfest"
     return HttpResponse(message)
